5b5b6494bb76f0eac5143c6ef2766aa9c0280672
sabbirsr <rakibhasansabbir@yahoo.com>
2017-08-04 01:05:20 +0600

    Add input from

--- src/main/java/bd/ac/seu/cheesemvc/controller/CheeseController.java
+++ src/main/java/bd/ac/seu/cheesemvc/controller/CheeseController.java
@@ -1,22 +1,46 @@
 package bd.ac.seu.cheesemvc.controller;
 
+import org.omg.CORBA.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 /**
  * Created by R@kib Hasan Sabbir
  */
 
 @Controller
-@RequestMapping("rakib")
+@RequestMapping("names")
 public class CheeseController {
+    static ArrayList<String> list1 = new ArrayList<>();
+
 
     @RequestMapping(value = "")
     public String index(Model model){
+
+
+        model.addAttribute("name",list1);
         model.addAttribute("title","Welcome Rakib Hasan");
         return "Cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddNamesFrom(Model model){
+        model.addAttribute("title","Add Names");
+        return "Cheese/add";
+
+    }
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddNamesFrom(@RequestParam String names){
+
+        list1.add(names);
+        return "redirect:";
     }
 
 
